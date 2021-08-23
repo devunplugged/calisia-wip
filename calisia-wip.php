7e6f1e38651f0822400ed532b811725af4907cfb
devunplugged <66313338+devunplugged@users.noreply.github.com>
2021-08-23 12:28:01 +0200

    cron jobs
    - cron jobs are no longer blocked

--- calisia-wip.php
+++ calisia-wip.php
@@ -16,11 +16,11 @@ class CalisiaWip{
         if(is_user_logged_in())
             return;
 
-        
-        $pageNames = explode("/",$_SERVER['REQUEST_URI']);
-        if(isset($pageNames[1]) && ($pageNames[1] == 'login' || $pageNames[1] == 'wp-admin' || isset($_GET['wc-api'])))
-            return;
+        $url = explode('?', $_SERVER['REQUEST_URI'], 2);
+        $pageNames = explode("/",$url[0]);
 
+        if(isset($pageNames[1]) && ($pageNames[1] == 'login' || $pageNames[1] == 'wp-admin' || $pageNames[1] == 'wp-cron.php'  || isset($_GET['wc-api'])))
+            return;
 
         include  __DIR__ . '/templates/wip.php';
         die;
